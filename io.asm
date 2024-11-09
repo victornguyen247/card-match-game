55041d0ddbf08cb82f16d73e16144289fdf757e7
Pham15 <phamalex05@gmail.com>
2024-11-09 16:33:26 -0600

    Made the board bigger to allow for expressions and added numbered cells

--- io.asm
+++ io.asm
@@ -1,13 +1,14 @@
 # io.asm
 .data
     prompt1: .asciiz "Enter first card positions (0->15): "
-    prompt2: .asciiz "Enter  second card positions (0->15): "
-    invalidMsg:     .asciiz "\nInvalid input. Try again.\n"
+    prompt2: .asciiz "Enter second card positions (0->15): "
+    invalidMsg: .asciiz "\nInvalid input. Try again.\n"
     newline: .asciiz "\n"
-    space: .asciiz " "
-    hidden: .asciiz "*"
-    border_line: .asciiz "\n+---+---+---+---+\n"
-    row_start: .asciiz "|"
+    space: .asciiz "   "
+    hidden: .asciiz " * "
+    border_line: .asciiz "\n+---------+---------+---------+---------+\n"
+    row_start_single: .asciiz "|  "    # For single digit numbers
+    row_start_double: .asciiz "| "     # For double digit numbers
 
 .text
     .globl display_board
@@ -33,10 +34,7 @@ display_board:
     syscall
     
 print_row:
-    # Print row start
-    la $a0, row_start
-    li $v0, 4
-    syscall
+    # Print row start (handled in print_card now)
     
     # Print 4 cards in current row
     li $t0, 0          # Column counter
@@ -51,49 +49,61 @@ print_card:
     add $t3, $t3, $s1
     lb $t4, ($t3)
     
+    # Determine which row start to use based on position number
+    li $t5, 10         # Compare with 10 to check if double digit
+    blt $s1, $t5, print_single_start
+    la $a0, row_start_double
+    j print_row_start
+print_single_start:
+    la $a0, row_start_single
+print_row_start:
+    li $v0, 4
+    syscall
+    
     # Print space before card
     la $a0, space
     li $v0, 4
     syscall
     
-    # If revealed, print number, otherwise print *
-    beqz $t4, print_hidden
+    # If revealed, print number; otherwise, print cell position
+    beqz $t4, print_position
     
-    # Print card value
+    # Print card value if revealed
     move $a0, $t2
     li $v0, 1
     syscall 
-    j print_space
+    j print_end_space
     
-print_hidden:
-    la $a0, hidden
-    li $v0, 4
+print_position:
+    # Print cell number if hidden
+    li $v0, 1
+    move $a0, $s1
     syscall
-    
-print_space:    
+
+print_end_space:    
     # Print space after card
     la $a0, space
     li $v0, 4
     syscall
-        
-    # Print row end
-    la $a0, row_start
-    li $v0, 4
-    syscall
     
-    # Print row end if at last column
+    # Move to the next column
     addi $t0, $t0, 1   # Increment column counter
     addi $s1, $s1, 1   # Increment position counter
     
-    blt $t0, 4, print_card
+    # Check if we're at the end of the row
+    bne $t0, 4, print_card
     
+    # Print the closing separator "|" at the end of the row
+    la $a0, row_start_single
+    li $v0, 4
+    syscall
     
     # Print row border
     la $a0, border_line
     li $v0, 4
     syscall
     
-    # Move to next row
+    # Move to the next row
     addi $s0, $s0, 1   # Increment row counter
     blt $s0, 4, print_row
     
