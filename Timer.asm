d256f568136ad31914d27c41d7ec76561913e89b
victornguyen247 <44254369+victornguyen247@users.noreply.github.com>
2024-11-10 00:30:58 -0600

    Update Timer.asm

--- Timer.asm
+++ Timer.asm
@@ -1,42 +1,41 @@
 .data
-    # Timer variables
-    startTime:      .word 0
-    currentTime:    .word 0
-    elapsedTime:    .word 0
-    timeMsg:        .asciiz "Time elapsed: "
-    secondsMsg:     .asciiz " seconds\n"
+    	startTime:      .word 0
+    	currentTime:    .word 0
+    	elapsedTime:    .word 0
+    	timeMsg:        .asciiz "Time elapsed: "
+    	secondsMsg:     .asciiz " seconds\n"
 .text 
-.globl startTimer
-.globl updateTimer
+	.globl startTimer
+	.globl updateTimer
 # Timer module
 startTimer:
-    li $v0, 30             # Get system time
-    syscall
-    sw $a0, startTime      # Save start time
-    jr $ra
+    	li $v0, 30             	# Get system time
+    	syscall
+    	sw $a0, startTime      	# Save start time
+    	jr $ra
 
 updateTimer:
-    li $v0, 30             # Get current time
-    syscall
-    sw $a0, currentTime
+    	li $v0, 30             	# Get current time
+    	syscall
+    	sw $a0, currentTime
     
-    # Calculate elapsed time
-    lw $t0, startTime
-    sub $t1, $a0, $t0
-    div $t1, $t1, 1000    # Convert to seconds
-    sw $t1, elapsedTime
+    	# Calculate elapsed time
+    	lw $t0, startTime
+    	sub $t1, $a0, $t0
+    	div $t1, $t1, 1000    	# Convert to seconds
+    	sw $t1, elapsedTime
     
-    # Display time
-    li $v0, 4
-    la $a0, timeMsg
-    syscall
+    	# Display time messenger
+    	li $v0, 4
+    	la $a0, timeMsg
+    	syscall
+    	# print time value
+    	li $v0, 1
+    	lw $a0, elapsedTime
+    	syscall
+    	# print senconds
+    	li $v0, 4
+    	la $a0, secondsMsg
+    	syscall
     
-    li $v0, 1
-    lw $a0, elapsedTime
-    syscall
-    
-    li $v0, 4
-    la $a0, secondsMsg
-    syscall
-    
-    jr $ra
+    	jr $ra
